e1cc34e62fa54c1b00fa0094d3618c6e784a1ed2
Thorin Butler <thorin.butler@gmail.com>
2017-03-21 21:48:07 -0500

    okay, I hate to punk out like this, but I made a tiny, and I mean tiny, change in following along with the tutorial.  I just spent the better part of the day coding Java.  So, I've done my coding today.  I just need to keep it up.  Also, runny nose for some reason.

--- scripts.js
+++ scripts.js
@@ -24,7 +24,7 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height)
-    const pixels = ctx.getImageData( 0, 0, width, height);
+    let pixels = ctx.getImageData( 0, 0, width, height);
   }, 16);
 }
 
