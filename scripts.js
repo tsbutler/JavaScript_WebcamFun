4855ff171e0f211e689f6a0441b25c2dbea2a15a
Thorin Butler <thorin.butler@gmail.com>
2017-03-23 21:36:46 -0500

    added in an rgbSplit function for another visual effect

--- scripts.js
+++ scripts.js
@@ -27,7 +27,8 @@ function paintToCanvas() {
     // take the pixels out
     let pixels = ctx.getImageData( 0, 0, width, height);
     // modify them
-    pixels = redEffect(pixels);
+    // pixels = redEffect(pixels);
+    pixels = rgbSplit(pixels);
     // put the pixels back
     ctx.putImageData(pixels, 0, 0);
   }, 16);
@@ -52,6 +53,15 @@ function redEffect(pixels){
     pixels.data[i + 0] = pixels.data[i + 0] + 100; // red
     pixels.data[i + 1] = pixels.data[i + 1] - 50; // green
     pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // blue
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels){
+  for(let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i - 150] = pixels.data[i + 0]; // red
+    pixels.data[i + 100] = pixels.data[i + 1]; // green
+    pixels.data[i - 150] = pixels.data[i + 2]; // blue
   }
   return pixels;
 }
